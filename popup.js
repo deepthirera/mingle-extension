58317ff95ba9512fc683b9d97b66d3a5309cd158
deepthi <deepthirera@gmail.com>
2012-07-09 09:54:15 +0530

    with close action

--- popup.js
+++ popup.js
@@ -5,9 +5,9 @@
 //document.getElementById("content").getElementsByTagName("img")
 var imgs = document.getElementById("content").getElementsByTagName("img");
 
-  e = document.getElementById("card_show_container")
-e.parentNode.removeChild(e)
-for(var i = 0; i < imgs.length; ++i) {
+var  e = document.getElementById("card_show_container");
+//e.parentNode.removeChild(e)
+var abc=[]
        // alert(imgs[i].src);
 //  var newImg = document.createElement("img");
 
@@ -16,16 +16,36 @@ for(var i = 0; i < imgs.length; ++i) {
 //newImg.height = 100;
 //newImg.width = 100;
 //document.body.appendChild(newImg);
-var j = document.createElement("img");
-j.src = imgs[i].src;
-j.height = 500;
-j.width = 500;
-var a = document.createElement('a');
-a.appendChild(j);
-a.href = j.src;
-a.rel = "lightbox[Stories]"
-document.body.appendChild(a);
+//var j = document.createElement("img");
+//j.src = imgs[i].src;
+//j.height = 500;
+//j.width = 500;
+var childElements = [];
+////var parent = imgs.item(0).parentNode;
+for(var i=0; i<imgs.length; i++){
+var j=imgs.item(i); 
+////var par = j.parentNode;
+var abcd = document.createElement('a');
+abcd.href = j.src;
+abcd.rel = "lightbox[Stories]";
+abcd.innerHTML=imgs[i].src;
+//newchild = abcd
+////par.removeChild(j);
+////console.log(par.childNodes.length);
+//par.appendChild(abcd);
+childElements[i] = abcd;
+imgs[i].parentNode.replaceChild(abcd,j);
 }
+//parent.appendChild(childElements[0]);
+
+
+
+var lk = document.createElement('link');
+lk.rel = "stylesheet";
+lk.href = chrome.extension.getURL('lightbox.css');
+lk.type = "text/css";
+lk.media = "screen";
+document.getElementsByTagName('head')[0].appendChild(lk); 
 var script = document.createElement('script');
 var script1 = document.createElement('script');
 var script2 = document.createElement('script');
@@ -36,13 +56,15 @@ script2.type = 'text/javascript';
 script3.type = 'text/javascript';
 script.src = chrome.extension.getURL('prototype.js');
 document.getElementsByTagName('head')[0].appendChild(script); 
-script1.src = chrome.extension.getURL('lightbox.js');
-document.getElementsByTagName('head')[0].appendChild(script1); 
 script2.src = chrome.extension.getURL('scriptaculous.js?load=effects');
 document.getElementsByTagName('head')[0].appendChild(script2); 
 script3.src = chrome.extension.getURL('effects.js');
 document.getElementsByTagName('head')[0].appendChild(script3);
+script1.src = chrome.extension.getURL('lightbox.js');
+document.getElementsByTagName('head')[0].appendChild(script1); 
 var lk = document.createElement('link');
+
+
 
 //<link rel="stylesheet" href="lightbox.css" type="text/css" media="screen">
 // document.writeln("<link rel='stylesheet' href='extensions/lightbox.css' type='text/css' media='screen' />");
